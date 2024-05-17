663c1b232f734ee8fe617d8bdf957c6411bd5ea6
umangnaith <152839736+umangnaith@users.noreply.github.com>
2024-05-17 13:47:53 +0530

    Department Table

---
+++ Dataset/Dept_table.sql
@@ -0,0 +1,9 @@
+create table dept(
+    dep_id int,
+    dep_name varchar(20)
+);
+insert into dept values(100,'Analytics');
+insert into dept values(200,'IT');
+insert into dept values(300,'HR');
+insert into dept values(400,'Text Analytics');
+select * from dept;
